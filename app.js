5b5ceba03e4d57cad1ae403dfb6548009fc894b8
CodeNinja48 <159473577+CodeNinja48@users.noreply.github.com>
2024-02-10 21:41:21 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const a = () =>{
+    let x = 'swift'
+    let y = 'C++'
+    let z = 'Kotlin'
+    document.getElementById('first').innerHTML = x;
+    document.getElementById('second').innerHTML = y;
+    document.getElementById('third').innerHTML = z;
+
+}
+a();
+console.log(a);
+const b = () =>{
+
+    let x = 'Javascript';
+    let y = 'Java';
+    let z = 'Ruby';
+    document.getElementById('first').innerHTML = x;
+    document.getElementById('second').innerHTML = y;
+    document.getElementById('third').innerHTML = z
+
+}
+b();
+console.log(b);
